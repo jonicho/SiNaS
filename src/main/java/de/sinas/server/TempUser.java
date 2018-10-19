865809533d220727f7194929267196cd36aac489
Jonas Keller <jonas.richardd@gmail.com>
2018-10-19 21:29:13 +0200

    remove setters for ip and port from TempUser
    setters for ip and port make no sense here

--- src/main/java/de/sinas/server/TempUser.java
+++ src/main/java/de/sinas/server/TempUser.java
@@ -1,74 +1,57 @@
 package de.sinas.server;
 
-import java.security.Key;
-import java.security.PublicKey;
-
 import javax.crypto.SecretKey;
 
 public class TempUser {
-    private String ip;
-    private int port;
-    private SecretKey aesKey;
-    private SecretKey rsaKey;
+	private String ip;
+	private int port;
+	private SecretKey aesKey;
+	private SecretKey rsaKey;
 
-    public TempUser(String pClientIP,int pClientPort) {
-        ip = pClientIP;
-        port = pClientPort;
-    }
+	public TempUser(String pClientIP, int pClientPort) {
+		ip = pClientIP;
+		port = pClientPort;
+	}
 
-    /**
-     * @return the aesKey
-     */
-    public SecretKey getAesKey() {
-        return aesKey;
-    }
-    
-    /**
-     * @return the ip
-     */
-    public String getIp() {
-        return ip;
-    }
+	/**
+	 * @return the aesKey
+	 */
+	public SecretKey getAesKey() {
+		return aesKey;
+	}
 
-    /**
-     * @return the port
-     */
-    public int getPort() {
-        return port;
-    }
+	/**
+	 * @param aesKey the aesKey to set
+	 */
+	public void setAesKey(SecretKey aesKey) {
+		this.aesKey = aesKey;
+	}
 
-    /**
-     * @return the rsaKey
-     */
-    public SecretKey getRsaKey() {
-        return rsaKey;
-    }
+	/**
+	 * @return the ip
+	 */
+	public String getIp() {
+		return ip;
+	}
 
-    /**
-     * @param aesKey the aesKey to set
-     */
-    public void setAesKey(SecretKey aesKey) {
-        this.aesKey = aesKey;
-    }
+	/**
+	 * @return the port
+	 */
+	public int getPort() {
+		return port;
+	}
 
-    /**
-     * @param ip the ip to set
-     */
-    public void setIp(String ip) {
-        this.ip = ip;
-    }
-    
-    /**
-     * @param port the port to set
-     */
-    public void setPort(int port) {
-        this.port = port;
-    }
+	/**
+	 * @return the rsaKey
+	 */
+	public SecretKey getRsaKey() {
+		return rsaKey;
+	}
 
-    /**
-     * @param rsaKey the rsaKey to set
-     */
-    public void setRsaKey(SecretKey rsaKey) {
-        this.rsaKey = rsaKey;
-    }
+	/**
+	 * @param rsaKey the rsaKey to set
+	 */
+	public void setRsaKey(SecretKey rsaKey) {
+		this.rsaKey = rsaKey;
+	}
 }
