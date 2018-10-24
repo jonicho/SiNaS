f04897f14911f52040a5a6e1a77d16247506f32a
Jonas Keller <jonas.richardd@gmail.com>
2018-10-24 18:36:20 +0200

    launch client in ClientLauncher

--- src/main/java/de/sinas/client/ClientLauncher.java
+++ src/main/java/de/sinas/client/ClientLauncher.java
@@ -1,7 +1,10 @@
 package de.sinas.client;
 
+import de.sinas.net.PROTOCOL;
+
 public class ClientLauncher {
 
 	public static void main(String[] args) {
+		AppClient appClient = new AppClient("localhost", PROTOCOL.PORT);
 	}
 }
