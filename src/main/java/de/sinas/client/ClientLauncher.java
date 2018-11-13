6af88929499cced992a487c4db838e45d25fafba
Jonas Keller <jonas.richardd@gmail.com>
2018-11-13 09:13:45 +0100

    use the system's look and feel

--- src/main/java/de/sinas/client/ClientLauncher.java
+++ src/main/java/de/sinas/client/ClientLauncher.java
@@ -1,12 +1,18 @@
 package de.sinas.client;
 
 import javax.swing.JDialog;
+import javax.swing.UIManager;
 
 import de.sinas.client.gui.LoginDialog;
 
 public class ClientLauncher {
 
 	public static void main(String[] args) {
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		try {
 			LoginDialog dialog = new LoginDialog();
 			dialog.setTitle("SiNaS - Login");
